1cd48d7529e90fb78f3f59352229be6469ce6fa8
Thiago Pereira <cmtepereirabr@gmail.com>
2023-03-04 17:15:29 -0300

    feat: all routes imported to index

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,42 @@
 */
 
 // Importando o express para o projeto e configurando na variavel app
+
 import express, {Request, Response} from 'express';
 const app = express();
 
 // Importando o body-parser para o projeto
+
 import bodyParser from 'body-parser'; 
 
 // Configurando o body parser para recebermos os parametros via url
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 
 // Importando o Dotenv para o projeto
 import 'dotenv/config';
+
+// Importando as rotas para o index e passando no express
+
+import { userRouter } from './User/userRoute';
+app.use(userRouter);
+import { rolesRouter } from './Roles/rolesRoute';
+app.use(rolesRouter);
+import { deptRouter } from './Departments/departamentRoute';
+app.use(deptRouter);
+import { deptEmpRouter } from './dept-employee/deptempRoute';
+app.use(deptEmpRouter);
+import { employeesRouter } from './Employees/employeesRoute';
+app.use(employeesRouter);
+import { permissionRouter } from './Permissions/permissionRoute';
+app.use(permissionRouter);
+import { rolePermRouter } from './roles-permission/rolePermRoute';
+app.use(rolePermRouter);
+import { salRouter } from './Salary/salaryRoute';
+app.use(salRouter);
+
 
 // Configurando o servidor
 const port = process.env.API_PORT;
