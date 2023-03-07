d5d2c0b93b0c53e1ee45d3f4be08b9425ebfb0e9
Thiago Pereira <cmtepereirabr@gmail.com>
2023-03-07 13:56:28 -0300

    refactor: changed the name of main index file

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,30 @@
  *      	Autor: Thiago Pereira
  */
 
-// Importando o express para o projeto e configurando na variavel app
+// Importando o AdminJS para o projeto
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express"
 
+// Importando o express para o projeto 
 import express, { Request, Response } from "express";
-const app = express();
 
 // Importando o body-parser para o projeto
-
 import bodyParser from "body-parser";
+
+// Importando o Dotenv para o projeto
+import "dotenv/config";
+
+// Importando o Cors para o projeto para permitir acesso do front-end
+/*
+import cors from "cors";
+app.use(cors());
+*/
+
+// Configurando o express para comunicar com o AdminJS
+const app = express();
+const admin = new AdminJS({});
+const adminRouter = AdminJSExpress.buildRouter(admin);
+app.use(admin.options.rootPath, adminRouter);
 
 // Configurando o body parser para recebermos os parametros via url
 
@@ -19,45 +35,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Importando o Dotenv para o projeto
-
-import "dotenv/config";
-
-// Importando o Cors para o projeto para permitir acesso do front-end
-
-import cors from "cors";
-app.use(cors());
-
 // Importando as rotas para o index e passando para o express via variavel app
 
 import { authRouter } from "./Authentication/authRouter";
 app.use(authRouter);
 
-import { userRouter } from "./User/userRoute";
+import { userRouter } from "./user/userRoute";
 app.use(userRouter);
 
-import { rolesRouter } from "./Roles/rolesRoute";
-app.use(rolesRouter);
-
-import { deptRouter } from "./Departments/departamentRoute";
+import { deptRouter } from "./departments/departamentRoute";
 app.use(deptRouter);
 
 import { deptEmpRouter } from "./dept-employee/deptempRoute";
 app.use(deptEmpRouter);
 
-import { employeesRouter } from "./Employees/employeesRoute";
+import { employeesRouter } from "./employees/employeesRoute";
 app.use(employeesRouter);
-
-import { permissionRouter } from "./Permissions/permissionRoute";
-app.use(permissionRouter);
-
-import { rolePermRouter } from "./roles-permission/rolePermRoute";
-app.use(rolePermRouter);
 
 // Configurando a criação do servidor com retorno de mensagem no console informando o sucesso
 const port = process.env.API_PORT;
 app.listen(port, () => {
-	console.log(`API Funcionando na porta ${port}`);
+	console.log(`AdminJS iniciado em htttp://localhost:${port}${admin.options.rootPath}`);
 });
 
 // Rota de teste da API
